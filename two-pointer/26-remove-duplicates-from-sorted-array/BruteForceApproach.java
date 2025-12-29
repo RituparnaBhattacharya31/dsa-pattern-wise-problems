a1eb7941302605423cbcb9279c16d8c3d74a669e
RituparnaBhattacharya31 <rituparnabhattachary0@gmail.com>
2025-12-29 13:07:37 +0530

    test

--- two-pointer/26-remove-duplicates-from-sorted-array/BruteForceApproach.java
+++ two-pointer/26-remove-duplicates-from-sorted-array/BruteForceApproach.java
@@ -3,19 +3,21 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class BruteForceApproach {
+
     public static int removeDuplicates(int[] nums) {
         List<Integer> unique = new ArrayList<>();
-        for(int num : nums) {
-            if(!unique.contains(num)) {
+        for (int num : nums) {
+            if (!unique.contains(num)) {
                 unique.add(num);
             }
         }
-        for(int i = 0; i < unique.size(); i++) {
+        for (int i = 0; i < unique.size(); i++) {
             nums[i] = unique.get(i);
         }
         return unique.size();
     }
-    public static void main(String []args) {
+
+    public static void main(String[] args) {
         int[] arr = new int[]{0, 1, 1, 2, 2, 2, 3, 3, 3, 3, 4};
         System.out.println("Updated length of the array is : " + removeDuplicates(arr));
     }
